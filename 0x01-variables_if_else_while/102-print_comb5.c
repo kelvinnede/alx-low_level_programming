04da45131585ddeac65b7c0ec4c516096224c0d6
Kelvin Nede <kelvinnede@gmail.com>
2023-10-06 05:00:45 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1_tens, num1_ones, num2_tens, num2_ones;
+
+	for (num1_tens = 0; num1_tens <= 9; num1_tens++)
+	{
+		for (num1_ones = 0; num1_ones <= 9; num1_ones++)
+		{
+			for (num2_tens = num1_tens; num2_tens <= 9; num2_tens++)
+			{
+				for (num2_ones = (num2_tens == num1_tens) ? num1_ones + 1 : 0; num2_ones <= 9; num2_ones++)
+				{
+					putchar(num1_tens + '0');
+					putchar(num1_ones + '0');
+					putchar(' ');
+					putchar(num2_tens + '0');
+					putchar(num2_ones + '0');
+
+					if (num1_tens < 9 || num1_ones < 8 || num2_tens < 9 || num2_ones < 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
