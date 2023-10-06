07e620212ffb113de22cdc3c17d2f6cce60d7bad
Kelvin Nede <kelvinnede@gmail.com>
2023-10-06 08:47:30 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,35 +2,27 @@
 
 /**
  * main - Entry point
- *
- * Description: Prints all possible combinations of two two-digit numbers
- *
+ * Description: Print all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num1_tens, num1_ones, num2_tens, num2_ones;
+	int num1, num2;
 
-	for (num1_tens = 0; num1_tens <= 9; num1_tens++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num1_ones = 0; num1_ones <= 9; num1_ones++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (num2_tens = num1_tens; num2_tens <= 9; num2_tens++)
-			{
-				for (num2_ones = (num2_tens == num1_tens) ? num1_ones + 1 : 0; num2_ones <= 9; num2_ones++)
-				{
-					putchar(num1_tens + '0');
-					putchar(num1_ones + '0');
-					putchar(' ');
-					putchar(num2_tens + '0');
-					putchar(num2_ones + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-					if (!(num1_tens == 9 && num1_ones == 8 && num2_tens == 9 && num2_ones == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
@@ -39,4 +31,3 @@ int main(void)
 
 	return (0);
 }
-
