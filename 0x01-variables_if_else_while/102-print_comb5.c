5c9a8d1554c0306523c12c9bda9f848e158318c0
Kelvin Nede <kelvinnede@gmail.com>
2023-10-06 05:10:23 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,33 +9,33 @@
  */
 int main(void)
 {
-    int num1_tens, num1_ones, num2_tens, num2_ones;
+	int num1_tens, num1_ones, num2_tens, num2_ones;
 
-    for (num1_tens = 0; num1_tens <= 9; num1_tens++)
-    {
-        for (num1_ones = 0; num1_ones <= 9; num1_ones++)
-        {
-            for (num2_tens = num1_tens; num2_tens <= 9; num2_tens++)
-            {
-                for (num2_ones = (num2_tens == num1_tens) ? num1_ones + 1 : 0; num2_ones <= 9; num2_ones++)
-                {
-                    putchar(num1_tens + '0');
-                    putchar(num1_ones + '0');
-                    putchar(' ');
-                    putchar(num2_tens + '0');
-                    putchar(num2_ones + '0');
+	for (num1_tens = 0; num1_tens <= 9; num1_tens++)
+	{
+		for (num1_ones = 0; num1_ones <= 9; num1_ones++)
+		{
+			for (num2_tens = num1_tens; num2_tens <= 9; num2_tens++)
+			{
+				for (num2_ones = (num2_tens == num1_tens) ? num1_ones + 1 : 0; num2_ones <= 9; num2_ones++)
+				{
+					putchar(num1_tens + '0');
+					putchar(num1_ones + '0');
+					putchar(' ');
+					putchar(num2_tens + '0');
+					putchar(num2_ones + '0');
 
-                    if (num1_tens < 9 || num1_ones < 8 || num2_tens < 9 || num2_ones < 9)
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
+					if (num1_tens < 9 || num1_ones < 8 || num2_tens < 9 || num2_ones < 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
