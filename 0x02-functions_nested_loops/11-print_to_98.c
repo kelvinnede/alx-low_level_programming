a244b6774a1a496f2b6afeac584de20fdcb2d50a
Kelvin Nede <kelvinnede@gmail.com>
2023-10-10 04:00:40 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *  * print_to_98 - prints all natural numbers from n to 98
+ *   * @n: starting number
+ *    *
+ *     * Return: void
+ *      */
+void print_to_98(int n)
+{
+	    if (n <= 98) {
+		            for (int i = n; i <= 98; i++) {
+				                printf("%d", i);
+
+						            if (i != 98) {
+								                    printf(", ");
+										                }
+							            }
+			        } else {
+					        for (int i = n; i >= 98; i--) {
+							            printf("%d", i);
+
+								                if (i != 98) {
+											                printf(", ");
+													            }
+										        }
+						    }
+
+	        printf("\n");
+}
