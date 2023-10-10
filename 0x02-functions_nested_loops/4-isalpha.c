03d7b2100f83957036541e27824082c5e7320fe2
Kelvin Nede <kelvinnede@gmail.com>
2023-10-10 06:14:00 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,11 @@
-int _isalpha(int c)
-{
-	    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
+#include "main.h"
 
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to check
+ *
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
+ */
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
