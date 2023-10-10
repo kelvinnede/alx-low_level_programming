05168515ea93fbbee144e7ae598f11b150c2917b
Kelvin Nede <kelvinnede@gmail.com>
2023-10-10 08:53:16 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,26 +7,21 @@
  */
 void print_to_98(int n)
 {
-	int i;
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n < 98; n++)
 		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
+			printf("%d, ", n);
 		}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (; n > 98; n--)
 		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
+			printf("%d, ", n);
 		}
 	}
 
-	printf("\n")
+	printf("%d\n", n);
 }
