83b0425c1ba045f59776c6423afaa3a52d37fe6b
Kelvin Nede <kelvinnede@gmail.com>
2023-10-10 04:37:17 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,10 @@
 int _putchar(char c);
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	_putchar('_');
