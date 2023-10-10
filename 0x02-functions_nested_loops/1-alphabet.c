dff9aa6eaa88ff5908d717e4513dfa373b48496b
Kelvin Nede <kelvinnede@gmail.com>
2023-10-10 03:36:52 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void _putchar(char c)
+{
+	    write(1, &c, 1);
+}
+
+void print_alphabet(void)
+{
+	    char letter = 'a';
+
+	        while (letter <= 'z')
+			    {
+				            _putchar(letter);
+					            letter++;
+						        }
+
+		    _putchar('\n');
+}
+
+int main(void)
+{
+	    print_alphabet();
+	        return 0;
+}
