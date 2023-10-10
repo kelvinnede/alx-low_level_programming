2fc7e5b87d1487d2706167583735e15b370d0f9e
Kelvin Nede <kelvinnede@gmail.com>
2023-10-10 04:10:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,23 @@
-#include <unistd.h>
+#include <stdio.h>
 
+int _putchar(char c);
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	    write(1, "_putchar\n", 9);
-	        return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
