e61eccb4cf4ffaf97c9df6ae8057c068e23d91b1
Kelvin Nede <kelvinnede@gmail.com>
2023-10-12 07:07:07 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,23 +6,21 @@
  */
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	else
+
+	for (i = 0; i < n; i++)
 	{
-		int i, j;
-
-		for (i = 0; i < n; i++)
-
+		for (j = 0; j < i; j++)
 		{
-			for (j = 0; j < i; j++)
-			{
-				-putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
+			-putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
