fc3817341f69d5bf8a8afa3386717f8938f984c2
Kelvin Nede <kelvinnede@gmail.com>
2023-10-12 06:24:57 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,10 +6,19 @@
  */
 void print_line(int n)
 {
-	if (n > 0)
+	int i;
+
+	if (n <= 0)
 	{
-		for (int i = 0; i < n; i++)
-			_putchar('_');
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
 }
