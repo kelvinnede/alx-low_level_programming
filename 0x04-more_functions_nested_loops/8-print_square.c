b97fe4a5d1413e9c471ed015a1158411b0e656c5
Kelvin Nede <kelvinnede@gmail.com>
2023-10-12 08:22:14 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,23 +12,12 @@ void print_square(int size)
 		return;
 	}
 
-	int i, j;
-
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (int j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
 		_putchar('\n');
 	}
-}
-
-/* Example of usage in main.c file */
-int main(void)
-{
-	print_square(2);
-	print_square(10);
-	print_square(0);
-	return (0);
 }
