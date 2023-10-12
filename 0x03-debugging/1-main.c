cd18170b66e17c5bc1bbbe8071d9a8640d871ec1
Kelvin Nede <kelvinnede@gmail.com>
2023-10-12 10:22:14 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,11 +15,11 @@ int main(void)
 
 	/*
 	 * Commented out the code causing the infinite loop
-	 * while (i < 10)
-	 * {
-	 *     putchar(i);
-	 * }
-	 */
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
