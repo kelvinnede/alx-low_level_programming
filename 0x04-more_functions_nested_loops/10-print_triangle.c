ac71696b1dc528133e6a834bb66a18bfe1230d76
Kelvin Nede <kelvinnede@gmail.com>
2023-10-13 04:49:43 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,36 +1,31 @@
 #include "main.h"
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-
-/**
- * print_triangle - prints a triangle of a given size
+ * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
 void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		_putchar('\n');
-		return;
+	_putchar('\n');
+	}
+	else
+	{
+	int i, j;
+
+	for (i = 1; i < size; i++)
+	{
+	for (j = 1; j < size; j++)
+	{
+	_putchar(' ');
 	}
 
-	for (int i = 0; i < size; ++i)
+	for (j = 1; j <= i; j++)
 	{
-		for (int j = 0; j < size; ++j)
-		{
+	_putchar('#');
+	}
 
-			_putchar('#');
-		}
-		_putchar('\n');
+	_putchar('\n');
+	}
 	}
 }
