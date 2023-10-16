aeca76850048a2ab0a8a17b04135100646d76bff
Kelvin Nede <kelvinnede@gmail.com>
2023-10-16 06:08:13 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+		i++;
+
+	while (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > (INT_MAX - (s[i] - '0')) / 10)
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
+}
