28bcf4c1926d70d6a349d7b2fd9ba76c53c4194f
Kelvin Nede <kelvinnede@gmail.com>
2023-10-16 01:52:21 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 /* Function prototypes */
 void reset_to_98(int *n);
-void swap_int(int *a, *b);
+void swap_int(int *a, int *b);
 int _putchar(char c);
 int _strlen(char *s);
 void _puts(char *str);
