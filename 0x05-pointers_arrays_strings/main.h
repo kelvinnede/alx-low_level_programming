3fed1114d5efb01f9a98cfdd101a0b4b4482a8fb
Kelvin Nede <kelvinnede@gmail.com>
2023-10-16 01:49:44 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,13 +3,13 @@
 
 /* Function prototypes */
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
+void swap_int(int *a, *b);
 int _putchar(char c);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts2(char *str)
+void puts2(char *str);
 
 #define UNUSED(x) (void)(x)
 
