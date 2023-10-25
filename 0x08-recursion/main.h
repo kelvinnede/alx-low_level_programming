7613df085c721037731df219caad1f9d87b95f40
Kelvin Nede <kelvinnede@gmail.com>
2023-10-25 02:57:09 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,6 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _sqrt_helper(int n, int i);
 int is_prime_recursive(int n, int i);
+int check_palindrome(char *s, int start, int end);
 
 #endif /* MAIN_H */
