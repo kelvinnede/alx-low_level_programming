15d2e1a22a8ea44c2e87f2652d4493af0f0ea901
Kelvin Nede <kelvinnede@gmail.com>
2023-10-27 08:39:39 -0700

    2-strchr.

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char *_strchr(const char *s, int c) {
+    while (*s != '\0') {
+        if (*s == c) {
+            return (char *)s;
+        }
+        s++;
+    }
+
+    return NULL;
+}
