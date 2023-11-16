ae18d3686f058b621248ac9ed9bd6ed35bd22293
Kelvin Nede <kelvinnede@gmail.com>
2023-11-16 03:29:40 -0800

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/* Structure definition for linked list node */
-typedef struct list_s
-{
-    char *str;
-    int len;
-    struct list_s *next;
-} list_t;
-
 /* Function prototype for printing linked list */
 size_t print_list(const list_t *h);
 int _putchar(char c);
