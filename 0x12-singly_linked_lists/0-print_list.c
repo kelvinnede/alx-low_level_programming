4516a7b6ce65bde4859f19a946e8f5391b606cad
Kelvin Nede <kelvinnede@gmail.com>
2023-11-16 03:33:47 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,6 +4,16 @@
 #include "lists.h"
 
 /**
+ * _putchar - Custom putchar function.
+ * @c: The character to print.
+ * Return: Always 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * print_list - Prints all the elements of a list_t list.
  * @h: A pointer to the head of the list_t list.
  * Return: The number of nodes in the list.
