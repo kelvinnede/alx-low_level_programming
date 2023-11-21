079963c54bc7c3c413e0478ca12e48811e7cf8ad
Kelvin Nede <kelvinnede@gmail.com>
2023-11-21 00:19:19 -0800

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,5 +30,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+void print_address(const void *addr);
+void print_number(int n);
+void print_loop(const void *addr, int value);
 
 #endif /* LISTS_H */
