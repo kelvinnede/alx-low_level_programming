141d7c4ace71d86b8ceff7626794fc9562a9fb8e
Kelvin Nede <kelvinnede@gmail.com>
2023-11-23 01:15:57 -0800

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
 
 #endif /* MAIN_H */
+
