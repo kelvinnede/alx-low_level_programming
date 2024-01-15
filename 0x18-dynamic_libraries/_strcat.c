fb8f361fd28f0b9702d341acef1b9980fa1ba50f
Kelvin Nede <kelvinnede@gmail.com>
2024-01-15 04:25:30 -0800

    _strcat.c

---
+++ 0x18-dynamic_libraries/_strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ *
+ * Return: Pointer to the destination string (dest).
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *original_dest = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+	return (original_dest);
+}
