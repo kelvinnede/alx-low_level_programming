e0fbbfd4ec47a52c9f7f87c5f9798daedb2a78c7
Kelvin Nede <kelvinnede@gmail.com>
2024-02-02 04:15:38 -0800

    hash_tables.h

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * * hash_djb2 - Implements the djb2 hash algorithm.
+ * * @str: The string to be hashed.
+ * *
+ * * Return: The hash value.
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
